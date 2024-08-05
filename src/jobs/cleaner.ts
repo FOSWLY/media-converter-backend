841864943b46959aeac0793442753c2fd26d0011
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-08-06 00:26:29 +0300

    rewrited to bullmq & added clean from db

--- src/jobs/cleaner.ts
+++ src/jobs/cleaner.ts
@@ -1,31 +1,41 @@
-import { parentPort } from "node:worker_threads";
-import process from "node:process";
-
-import * as path from "node:path";
+import path from "node:path";
 import { readdir, rmdir } from "node:fs/promises";
 
+import { Job } from "bullmq";
+
 import config from "../config";
-
 import { getCurrentDate } from "../libs/utils";
+import { log } from "../logging";
+import ConvertFacade from "../facades/convert";
 
-async function clean(clearPath: string) {
-  const currentDate = getCurrentDate();
-  const files = await readdir(clearPath);
-  return await Promise.all(
-    files
-      .filter((file) => file !== currentDate)
-      .map(
-        async (file) =>
-          await rmdir(path.join(clearPath, file), {
-            recursive: true,
-          }),
-      ),
-  );
+export default abstract class CleanerJob {
+  static async clean(clearPath: string) {
+    const currentDate = getCurrentDate();
+    const files = await readdir(clearPath);
+    return await Promise.all(
+      files
+        .filter((file) => file !== currentDate)
+        .map(
+          async (file) =>
+            await rmdir(path.join(clearPath, file), {
+              recursive: true,
+            }),
+        ),
+    );
+  }
+
+  static async processor() {
+    await CleanerJob.clean(path.join(config.app.publicPath, "media", "mp4")); // public
+    await CleanerJob.clean(path.join(__dirname, "../libs/converters/temp")); // temp
+
+    await new ConvertFacade().deleteByLessTime(new Date()); // remove all converts less date
+  }
+
+  static onFailed(job: Job | undefined, error: Error) {
+    log.error(`${job?.id} has failed with ${error.message}`);
+  }
+
+  static onCompleted(job: Job) {
+    log.info(`Job ${job.id} has completed!`);
+  }
 }
-
-await clean(path.join(config.app.publicPath, "media", "mp4")); // public
-await clean(path.join(__dirname, "../libs/converters/temp")); // temp
-
-// signal to parent that the job is done
-if (parentPort) parentPort.postMessage("done");
-else process.exit(0);
