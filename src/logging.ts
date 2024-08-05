4ec96afa8bad456588f90fc815f7428aa6c87509
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-08-06 00:31:56 +0300

    renamed * as path to path

--- src/logging.ts
+++ src/logging.ts
@@ -1,4 +1,4 @@
-import * as path from "node:path";
+import path from "node:path";
 import { pino, type TransportMultiOptions, type TransportTargetOptions } from "pino";
 
 import config from "./config";
