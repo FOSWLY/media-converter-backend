4e846abde538d88d0a67e120c0f67e1afa3ec4da
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-08-06 00:32:41 +0300

    removed old code

--- src/worker.ts
+++ src/worker.ts
@@ -1,33 +1,3 @@
-// import path from "node:path";
-
-// import Bree from "bree";
-
-// import { log } from "./logging";
-
-// export default new Bree({
-//   logger: log,
-//   root: path.join(__dirname, "jobs"),
-//   defaultExtension: "ts",
-//   jobs: [
-//     {
-//       name: "cleaner",
-//       interval: "at 02:00 am",
-//       timeout: 0,
-//     },
-//   ],
-//   errorHandler: (error, workerMetadata) => {
-//     // workerMetadata will be populated with extended worker information only if
-//     // Bree instance is initialized with parameter `workerMetadata: true`
-//     log.error(
-//       workerMetadata.threadId
-//         ? `There was an error while running a worker ${workerMetadata.name} with thread ID: ${workerMetadata.threadId}`
-//         : `There was an error while running a worker ${workerMetadata.name}`,
-//     );
-
-//     log.error(error);
-//   },
-// });
-
 /* eslint-disable @typescript-eslint/unbound-method */
 import { Queue, QueueBaseOptions, Worker } from "bullmq";
 
